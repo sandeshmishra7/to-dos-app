2625c6c0e153d28d4262d3b903492e5caa3471ce
SandeshM <sm03055603@gmail.com>
2024-01-26 14:16:49 +0530

    flex problem solved in notes

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 let inputVal = document.getElementById('name');
 let form = document.querySelector('form');
-let notesWrapper = document.querySelector('.notes-wrapper');
+let notes = document.querySelector('.notes');
 
 form.addEventListener('submit', formSubmit);
 
 function formSubmit(e) {
-
     e.preventDefault();
 
     var paragraph = document.createElement('p');
     var deleteBtn = document.createElement('div');
+    var notesWrapper = document.createElement('div');
+    notesWrapper.classList.add('notes-wrapper');
     deleteBtn.innerText = 'Delete Note';
     deleteBtn.classList.add('btn-style');
     paragraph.innerText = inputVal.value;
     paragraph.classList.add('p-style');
     notesWrapper.appendChild(paragraph);
     notesWrapper.appendChild(deleteBtn);
+    notes.appendChild(notesWrapper);
     inputVal.value = '';
 
     deleteBtn.addEventListener('click', function () {
