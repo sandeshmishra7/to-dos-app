4df6fe912e5edf76ca4f7a92713520dbe0809485
SandeshM <sm03055603@gmail.com>
2024-01-28 16:57:35 +0530

    checkbox event listener added

--- script.js
+++ script.js
@@ -7,30 +7,37 @@ form.addEventListener('submit', formSubmit);
 function formSubmit(e) {
     e.preventDefault();
 
+    // Creating elements
     var paragraph = document.createElement('p');
     var deleteBtn = document.createElement('div');
     var checkbox = document.createElement('input');
     var notesWrapper = document.createElement('div');
 
-    notesWrapper.classList.add('notes-wrapper');
-
     paragraph.innerText = inputVal.value.trim();
+
+    // Adding styling to the elements via css classes
+    notesWrapper.classList.add('notes-wrapper');
     paragraph.classList.add('p-style');
+    deleteBtn.classList.add('btn-style');
+    checkbox.classList.add('checkbox-style');
 
     deleteBtn.innerText = 'Delete Note';
-    deleteBtn.classList.add('btn-style');
 
-    checkbox.classList.add('checkbox-style');
     checkbox.type = "checkbox";
     checkbox.name = "name";
     checkbox.value = "value";
     checkbox.id = "id";
 
+    checkbox.addEventListener('click', function(){
+        paragraph.classList.toggle('cancel');
+    });
+
     if (inputVal.value.length > 20) {
         return alert('Please use less than 20 characters');
-    }
+    };
 
     if (inputVal.value.length != 0) {
+        // Appending elements to the structure
         notesWrapper.appendChild(paragraph);
         notesWrapper.appendChild(checkbox);
         notesWrapper.appendChild(deleteBtn);
@@ -46,5 +53,6 @@ function formSubmit(e) {
         notesWrapper.removeChild(paragraph);
         notesWrapper.removeChild(checkbox);
         notesWrapper.removeChild(deleteBtn);
+        notes.removeChild(notesWrapper);
     });
 }
